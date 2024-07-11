3fe0cc1f87558f6f2e7b9fe89995c36dda35da7d
MelkorDev <bahamondejosepablo@gmail.com>
2024-07-10 21:22:54 -0400

    Archivos no sincronizados y v0.4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //inicializar carrousel
 const carousel = new bootstrap.Carousel('#carousel')
 //inicializar tooltip
-$('[data-toggle="tooltip"]').tooltip();   
+$('[data-toggle="tooltip"]').tooltip;   
 
 
